91788bf004d698d6ab704656786bf88db95c0a47
fangxing <fangxing1996@126.com>
2021-07-24 17:53:42 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+class Person{
+    private:
+        int nAge;
+    public:
+        Person(){
+
+        }
+        Person(int x):nAge(x) {
+            cout << "construction!" << endl;
+        }
+        virtual int getAge(){
+
+            cout << "Person" << endl;
+            return nAge;
+        }
+        ~Person(){
+            cout << "deconstruction!" << endl;
+        }
+};
+int main(){
+    Person p(12);
+    Person q(21);
+    Person r(31);
+    cout << &p << ' ' << &q << endl;
+    cout << *((int*)&p) << ' ' << *((int*)&q) << ' ' << *((int*)&r) << endl;
+    cout << sizeof(int*) << endl << sizeof(char*) << endl;
+    return 0;
+}
